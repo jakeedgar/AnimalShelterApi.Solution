b9f084bf6f00efa246214e20d0225d6b7a363d69
Jake Edgar <jakeedgar1012@gmail.com>
2022-06-10 11:34:33 -0700

    add basic validation to animal class

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,12 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelter.Models
 {
     public class Animal
     {
+        [Required]
         public int AnimalId { get; set; }
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
+        [Required]
         public string Breed { get; set; }
+        [Required]
         public string Color { get; set; }
+        [Required]
         public int Age { get; set; }
+        [Required]
         public int Price { get; set; }
     }
 }
