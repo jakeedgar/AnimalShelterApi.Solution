4d1c36cb51b6b1d3f8920e5351d13fc901cb48d3
Jake Edgar <jakeedgar1012@gmail.com>
2022-06-10 16:28:21 -0700

    WIP still debugging, added applicationUser Model

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -1,48 +1,50 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelter.Models
 {
-    public class AnimalShelterContext : DbContext
+    public class AnimalShelterContext : IdentityDbContext<ApplicationUser>
     {
-        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
-            : base(options)
-        {
-        }
 
         public DbSet<Animal> Animals { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder)
+        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+            : base(options)
         {
-          builder.Entity<Animal>()
-              .HasData(
-                  new Animal 
-                  { 
-                    AnimalId = 1, 
-                    Name = "Sarah", 
-                    Breed = "Unicorn",
-                    Color = "Clear", 
-                    Age = 7, 
-                    Price = 1
-                  },
-                  new Animal 
-                  { 
-                    AnimalId = 2, 
-                    Name = "Nick", 
-                    Breed = "Dragon",
-                    Color = "Green", 
-                    Age = 108, 
-                    Price = 0
-                  },
-                  new Animal 
-                  { 
-                    AnimalId = 3, 
-                    Name = "Jeremy", 
-                    Breed = "Minotaur",
-                    Color = "Yellow", 
-                    Age = 12, 
-                    Price = 324
-                  }
-              );
+
         }
+        // protected override void OnModelCreating(ModelBuilder builder)
+        // {
+        //   builder.Entity<Animal>()
+        //       .HasData(
+        //           new Animal 
+        //           { 
+        //             AnimalId = 1, 
+        //             Name = "Sarah", 
+        //             Breed = "Unicorn",
+        //             Color = "Clear", 
+        //             Age = 7, 
+        //             Price = 1
+        //           },
+        //           new Animal 
+        //           { 
+        //             AnimalId = 2, 
+        //             Name = "Nick", 
+        //             Breed = "Dragon",
+        //             Color = "Green", 
+        //             Age = 108, 
+        //             Price = 0
+        //           },
+        //           new Animal 
+        //           { 
+        //             AnimalId = 3, 
+        //             Name = "Jeremy", 
+        //             Breed = "Minotaur",
+        //             Color = "Yellow", 
+        //             Age = 12, 
+        //             Price = 324
+        //           }
+        //       );
+        // }
     }
 }
---
+++ AnimalShelter/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AnimalShelter.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
